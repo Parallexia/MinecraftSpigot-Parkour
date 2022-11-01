04c683b21d50516568562f2818c4939e42c7e7c3
Parallexia <parallexia@outlook.com>
2022-11-01 22:03:46 +0800

    feat:项目搭建

---
+++ src/main/java/online/parallexia/mcplugin/parkour/Parkour.java
@@ -0,0 +1,18 @@
+package online.parallexia.mcplugin.parkour;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class Parkour extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
